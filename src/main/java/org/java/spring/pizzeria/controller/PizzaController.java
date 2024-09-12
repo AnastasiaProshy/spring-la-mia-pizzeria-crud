0f8ec22d092c9e53f57f3a8f80dc3fb66fe0b0ef
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-09-12 19:02:16 +0200

    Add pizza search functionality and update templates
    Implemented pizza search by name in the controller
    Updated pizza display logic
    Fixed HTML templates for pizza detail view

--- src/main/java/org/java/spring/pizzeria/controller/PizzaController.java
+++ src/main/java/org/java/spring/pizzeria/controller/PizzaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,7 +20,8 @@ public class PizzaController
 	
 	// repository field con autowired per d.i.
 	
-	@GetMapping
+	
+	@GetMapping()
 	public String index(Model model)
 	{
 		// take the data to deliver to books/index
@@ -30,5 +32,23 @@ public class PizzaController
 		
 		return "/pizzas/index";
 	}
+	
+	
+	@GetMapping ("/{id}")
+	public String show (@PathVariable("id") Integer id,  Model model)
+	{
+		model.addAttribute("pizza", repo.findById(id).get());
+		return "/pizzas/show";
+	}
+	
+	
+	@GetMapping ("/findByName/{name}")
+	public String findByName(@PathVariable("name") String name, Model model)
+	{
+		model.addAttribute("pizzas", repo.findByNameContains(name));
+		return "/pizzas/index";
+	}
+	
+	
 	
 }
--- src/main/java/org/java/spring/pizzeria/repo/PizzaRepository.java
+++ src/main/java/org/java/spring/pizzeria/repo/PizzaRepository.java
@@ -1,5 +1,7 @@
 package org.java.spring.pizzeria.repo;
 
+import java.util.List;
+
 import org.java.spring.pizzeria.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +9,8 @@ public interface PizzaRepository extends JpaRepository<Pizza, Integer>
 {
 	// Provides all basic CRUD operations
 	// With the option to add custom methods and features if needed
+	
+	public List<Pizza> findByNameContains(String name);
+	
+	
 }
